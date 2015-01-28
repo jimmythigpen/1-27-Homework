32b19a1174f21332b0cbb2adcbecbe12fcbc8447
Jimmy Thigpen <jmthigpen@me.com>
2015-01-27 19:40:24 -0500

    8PM updatE

--- main.js
+++ main.js
@@ -3,22 +3,46 @@
 
   $(document).ready(function() {
 
-    var myGitHubAddress = "https://api.github.com/users/jimmythigpen/repos";
-    var gitURL = myGitHubAddress + "?access_token=" + gitHubToken;
+    var myGitHubAddress = "https://api.github.com/users/jimmythigpen";
+    var repoAddToURL = "/repos";
+    var gitToken = "?access_token=" + gitHubToken;
+
+
+    var renderUserTemplate = _.template($('.user-info').text());
+    var userTemplate = $('.user-container');
+
+    var renderRepoTemplate = _.template($('.repo-info').text());
+    var repoTemplate = $('.repo-container');
+
+
+    //
+    // Repos AJAX Call
+    //
 
     $.ajax({
-      url: gitURL,
+      url: myGitHubAddress + repoAddToURL + gitToken,
       dataType: 'json'
     }).done(function(repoResults) {
-      console.log(repoResults);
       _.each(repoResults, function(item) {
         var data = {
           name: item.name
         };
-        console.log(data);
+        // console.log(data);
+        repoTemplate.append(renderRepoTemplate(data));
       })
     });
 
+    //
+    // User AJAX Call
+    //
+
+    $.ajax({
+      url: myGitHubAddress + gitToken,
+      dataType: 'json'
+    }).done(function(userResults) {
+      // console.log(userResults);
+      userTemplate.append(renderUserTemplate(userResults));
+    });
 
   });
 
